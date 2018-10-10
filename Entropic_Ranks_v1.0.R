a2923674d80fdd3a1612457f56b1d18b30cb189a
Hector - Xavier de Lastic <37729624+Hector-Xavier@users.noreply.github.com>
2018-10-10 12:42:49 +0300

    Update Entropic_Ranks_v1.0.R

--- Entropic_Ranks_v1.0.R
+++ Entropic_Ranks_v1.0.R
@@ -4,7 +4,7 @@ population_vector <- read.table(arguments[2],sep="\t",dec=".",header=TRUE)[,1]
 huge <- as.logical(sum(as.integer(arguments=="huge_feature_list")) > 0)
 
 message(arguments)
-message(huge_feature_list)
+message(huge)
 message(length(population_vector))
 library("RankProd")
 library("entropy")
